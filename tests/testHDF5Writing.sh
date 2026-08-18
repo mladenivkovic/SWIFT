beda3ba28c8436d4f66457569d9024ea55efec00
Elijah Cann <dc-cann2@login8b.pri.cosma.local>
2026-08-18 11:13:31 +0100

    Ammend shell file

---
+++ tests/testHDF5Writing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Creating initial conditions"
+python3 ./makeInput.py
+
+echo "Generating output"
+./testHDF5Writing 2>&1 | tee HDF5_writing.log
+
+# to add output checking
+# echo "Checking output"
+# python3 ./testSelectOutput.py
+
+# Do we want to clean up?
+# rm -f testHDF5Writing.hdf5 testHDF5Writing.xmf HDF5_writing.log 
+
+echo "Test passed"
