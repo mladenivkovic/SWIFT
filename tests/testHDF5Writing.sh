b2e2c32c717a82530c209c657af5a7eb0c7888c1
Elijah Cann <dc-cann2@login8b.pri.cosma.local>
2026-08-20 09:54:51 +0100

    Correct cleanup comments

--- tests/testHDF5Writing.sh
+++ tests/testHDF5Writing.sh
@@ -6,11 +6,7 @@ python3 ./makeInput.py
 echo "Generating output"
 ./testHDF5Writing 2>&1 | tee HDF5_writing.log
 
-# to add output checking
-# echo "Checking output"
-# python3 ./testSelectOutput.py
-
-# NOTE Do we want to clean up?
+# Clean up
 rm -f testHDF5Writing*.hdf5 testHDF5Writing.xmf HDF5_writing.log 
 
 echo "Test passed"
