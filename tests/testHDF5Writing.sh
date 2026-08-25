5d2a6f522b57808b8b97cbc4b07a658f30bb850f
Elijah Cann <dc-cann2@login8b.pri.cosma.local>
2026-08-25 10:22:38 +0100

    Clear out files from test suite

--- tests/testHDF5Writing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Generating output"
-./testHDF5Writing testHDF5Writing.ini 2>&1 | tee HDF5_writing.log
-
-# Clean up
-rm -f testHDF5Writing*.hdf5 testHDF5Writing.xmf HDF5_writing.log 
-
-echo "Test passed"
